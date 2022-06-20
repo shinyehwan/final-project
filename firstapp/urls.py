6bcbdb8dfd74810031a4d105c3756a21d8af7c0e
shinyehwan <jghisjl007@naver.com>
2022-06-20 18:29:21 +0900

    name 수정

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -6,9 +6,9 @@ app_name="firstapp"
 # 들어오고자 하는 경로: "127.0.0.1:8000/account/hello_world" <=> accountapp:firstpage_name
 
 urlpatterns = [
-    path("", index, name=""),
-    path("index.html", index, name=""),
-    path("1.html", first, name=""),
-    path("3.html", third, name="")
+    path("", index, name="firstpage"),
+    path("index.html", index, name="introduction"),
+    path("1.html", first, name="motivation"),
+    path("3.html", third, name="reading")
 ]
 
